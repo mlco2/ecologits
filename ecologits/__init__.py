22c2b82bd30166fe2f127b99c9051483369bb232
Vinisha <viba2022@gmail.com>
2026-05-13 12:05:20 -0700

    feat: export config helpers and new exception classes from top-level package

--- ecologits/__init__.py
+++ ecologits/__init__.py
@@ -1,7 +1,26 @@
 from ._ecologits import EcoLogits
+from .config import load_config_from_env, load_config_from_json
+from .exceptions import (
+    ConfigurationError,
+    EcoLogitsError,
+    InvalidProviderError,
+    ModelingError,
+    OpenTelemetryNotInstalledError,
+    ProviderNotInstalledError,
+    TracerInitializationError,
+)
 
 __version__ = "0.10.1"
 __all__ = [
     "EcoLogits",
-    "__version__"
+    "load_config_from_json",
+    "load_config_from_env",
+    "EcoLogitsError",
+    "TracerInitializationError",
+    "ModelingError",
+    "ProviderNotInstalledError",
+    "InvalidProviderError",
+    "ConfigurationError",
+    "OpenTelemetryNotInstalledError",
+    "__version__",
 ]
